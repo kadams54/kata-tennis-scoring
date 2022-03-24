a8d72613b02baa98fb498c7a321b2a7b3f15020e
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-24 09:49:53 -0400

    Failing test for tracking the game winner

--- python/tests/test_game.py
+++ python/tests/test_game.py
@@ -1,4 +1,5 @@
-from tennis_scoring.game import start, Score
+from tennis_scoring.game import start, Score, Player
 
 def test_start():
-    assert start() == {'player1': Score.LOVE, 'player2': Score.LOVE}
+    assert start() == {
+        Player.ONE: Score.LOVE, Player.TWO: Score.LOVE, 'winner': None}
