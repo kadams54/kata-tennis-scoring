cc136954c5444f570c10da6212a3214044171569
Kyle Adams <kadams54@users.noreply.github.com>
2022-03-28 10:14:46 -0400

    WIP

--- python/src/tennis_scoring/game.py
+++ python/src/tennis_scoring/game.py
@@ -11,4 +11,14 @@ class Score(Enum):
     FOURTY = 40
 
 def start():
-    return {Player.ONE: Score.LOVE, Player.TWO: Score.LOVE, 'winner': None}
+    player1_iterator = iter(Score)
+    player2_iterator = iter(Score)
+    return {
+        Player.ONE: (next(player1_iterator), player1_iterator),
+        Player.TWO: (next(player2_iterator), player2_iterator),
+        'winner': None
+    }
+
+def score(game, player):
+    (_, current_score_iter) = game[player]
+    return next(current_score_iter)
--- python/tests/test_game.py
+++ python/tests/test_game.py
@@ -1,4 +1,14 @@
-from tennis_scoring.game import (Player, Score, start)
+from tennis_scoring.game import (Player, Score, score, start)
 
 def test_start():
-    assert start() == {Player.ONE: Score.LOVE, Player.TWO: Score.LOVE, 'winner': None}
+    assert start() == {
+        Player.ONE: (Score.LOVE, iter(Score)),
+        Player.TWO: (Score.LOVE, iter(Score)),
+        'winner': None
+    }
+
+def test_score():
+    game = start()
+    (old_score, _) = game[Player.ONE]
+    new_score = score(game, Player.ONE)
+    assert new_score > old_score
